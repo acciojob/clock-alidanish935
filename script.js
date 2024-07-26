6474628984e90828913a6dfbd55c54cf2cefc03a
alidanish935 <114473927+alidanish935@users.noreply.github.com>
2024-07-26 23:16:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 //your JS code here. If required.
+function updateTimer() {
+	const timerElem = document.getElementById("timer");
+	const date = new Date();
+	const year = date.getFullYear();
+	const month = String(date.getMonth()+1);
+	const day = String(date.getDate());
+	const hour = date.getHours();
+	const day = String(date.getMinutes());
+	const day = String(date.getSeconds());
+	const formattedTime =`${month}/${day}/${year}`;
+	timerElem.textContent = formattedTime;
+	
+}
+updateTimer()
+
