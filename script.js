c7e8d8c678f3a01ea06913733293e42f493188ac
alidanish935 <114473927+alidanish935@users.noreply.github.com>
2024-07-26 23:37:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,14 +3,17 @@ function updateTimer() {
 	const timerElem = document.getElementById("timer");
 	const date = new Date();
 	const year = date.getFullYear();
-	const month = String(date.getMonth()+1);
-	const day = String(date.getDate());
+	const month = String(date.getMonth()+1).padStart(2, "0");
+	const day = String(date.getDate()).padStart(2,"0");
 	const hour = date.getHours();
-	const day = String(date.getMinutes());
-	const day = String(date.getSeconds());
-	const formattedTime =`${month}/${day}/${year}`;
+	const min = String(date.getMinutes()).padStart(2, "0");
+	const sec = String(date.getSeconds()).padStart(2,"0");
+	const ampm = hour>=12?"PM":"AM";
+	const formattedHr = hour%12|| 12;
+	const formattedTime =`${month}/${day}/${year}, ${formattedHr}:${min}:${sec} ${ampm} `;
 	timerElem.textContent = formattedTime;
 	
 }
 updateTimer()
+setInterval(updateTimer, 1000);
 
